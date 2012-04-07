eb2cef39aa4b189c79946053d89d365e0f29ed67
Kelly Becker <kbecker@kellybecker.me>
2012-04-07 13:50:31 -0600

    new page link generator

--- _bundle.php
+++ _bundle.php
@@ -18,34 +18,139 @@ class Bundle extends SQLBundle {
 	public function _on_portal_exception($path, $dir, $exception) {
 		$this->route(array('special-exception'), array($dir));
 	}
-	
-	public function page_array() {
+
+	public function page_structure($depth = 2, $html = false) {
+		if(is_string($depth)) $depth = (int) $depth;
 		$pages = $this->getPages();
-		
+
+		/**
+		 * The current URL
+		 */
+		$curr_url = $_SERVER['REDIRECT_URL'];
+		$curr_url_index = $curr_url === '/' ? '/index' : $curr_url.'/index';
+
+		/**
+		 * Get our array structure of pages
+		 */
 		$return = array();
-		foreach($pages as $page) {
-			$link = array('title' => $page->title, 'link'=>$page->url_on > 0 ? $page->url : '/'.$page->uri);
-			
-			$segs = explode('/', $page->uri);
-			$cseg = count($segs);
-			
-			if(in_array('index', $segs)) {
-				array_pop($segs);
-				$cseg--;
+		foreach($pages->order('`uri`', 'ASC') as $page) {
+
+			/**
+			 * Dont show the page in the NAV
+			 */
+			if($page->hidden > 0) continue;
+
+			/**
+			 * Explode the URI to get the segments
+			 */
+			$uri = !empty($page->uri) ? explode('/', $page->uri) : array(strtolower($page->title));
+
+			/**
+			 * Tell the array when it can build the page
+			 */
+			array_push($uri, '@---end');
+
+			/**
+			 * Process the placement in the array
+			 */
+			$arrayPiece = array(); $counter = 0;
+			foreach(array_reverse($uri) as $seg) {
+				if($counter === $depth) {
+					$arrayPiece = array();
+					break;
+				}
+
+				if($seg !== '@---end' && $seg !== 'index') {
+					$counter++;
+					if(isset($arrayPiece['selected']) && $arrayPiece['selected'])
+						$arrayPiece = array(
+							'children' => array($seg => $arrayPiece),
+							'selected' => true
+						);
+					else $arrayPiece = array(
+							'children' => array($seg => $arrayPiece),
+							'selected' => false
+						);
+					continue;
+				}
+
+				if('/'.$page->uri === $curr_url)
+					$selected = true;
+				else if('/'.$page->uri === $curr_url_index)
+					$selected = true;
+				else
+					$selected = false;
+
+				$arrayPiece = array(
+					'title' => $page->title,
+					'url' => $page->url_on ? $page->url : '/'.$page->uri,
+					'priority' => $page->priority,
+					'selected' => $selected
+				);
 			}
-			
-			$stack = array();
-			foreach(array_reverse($segs) as $i=>$seg) {
-				if($i < $cseg) $stack = array_merge(array('children' => $stack), $link);
-				$stack = array($seg => $stack);
-			}
-			
-			if($cseg < 1) $stack['index'] = $link;
-			
-			$return = e\array_merge_recursive_simple($stack, $return);
+
+			/**
+			 * Handle the top level index
+			 */
+			if($counter == 0) $arrayPiece = array('children' => array($seg => $arrayPiece));
+
+			$return = e\array_merge_recursive_simple($return, $arrayPiece);
 		}
-		
-		return array_reverse($return);
+
+		/**
+		 * By default the index is merged into the children so lets shift the array
+		 */
+		$return = array_shift($return);
+
+		/**
+		 * Prioritize
+		 */
+		$prioritize = function($array) use (&$prioritize) {
+			uasort($array, function($a, $b) {
+				if ($a['priority'] == $b['priority'])
+					return 0;
+				return ($a['priority'] < $b['priority']) ? -1 : 1;
+			});
+
+			foreach($array as &$segment) {
+				$segment['children'] = $prioritize($segment['children']); 
+			}
+
+			return $array;
+		};
+
+		$return = $prioritize($return);
+
+		if(!$html) return $return;
+
+		/**
+		 * Render array into html
+		 */
+		$render = function($array) use (&$render) {
+			static $depth = 0;
+			$depth++;
+
+			$html = "<ul class=\"depth-$depth\">";
+
+			foreach($array as $key => $segment) {
+				$html .= "<li>";
+
+				if(isset($segment['title']) && isset($segment['url']))
+					$html .= "<a class=\"".($segment['selected'] ? 'selected' : '')."\" href=\"$segment[url]\">$segment[title]</a>";
+				else
+					$html .= "<a class=\"".($segment['selected'] ? 'selected' : '')."\">".ucwords($key)."</a>";
+
+				if(isset($segment['children']))
+					$html .= $render($segment['children']);
+			}
+
+			$html .= "</ul>";
+
+			$depth--;
+			return $html;
+		};
+
+		return $render($return);
 	}
 	
 	public function route($path, $dir, $index = false) {
